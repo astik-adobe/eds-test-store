97f6ee4e34ad8e5ea337058524181b3ddcffb216
astik unagar <aunagar@adobe.com>
2025-03-29 20:18:06 +0530

    added accordion

--- scripts/configs.js
+++ scripts/configs.js
@@ -1,3 +1,5 @@
+import { getHref, getOrigin } from './scripts.js';
+
 const ALLOWED_CONFIGS = ['prod', 'stage', 'dev'];
 
 /**
@@ -8,7 +10,8 @@ const ALLOWED_CONFIGS = ['prod', 'stage', 'dev'];
  * @returns {string} - environment identifier (dev, stage or prod'.
  */
 export const calcEnvironment = () => {
-  const { host, href } = window.location;
+  const href = getHref();
+  const host = getOrigin();
   let environment = 'prod';
   if (href.includes('.aem.page') || host.includes('staging')) {
     environment = 'stage';
@@ -35,8 +38,7 @@ function buildConfigURL(environment) {
   if (env !== 'prod') {
     fileName = `configs-${env}.json`;
   }
-  const configURL = new URL(`${window.location.origin}/${fileName}`);
-  return configURL;
+  return new URL(`${getOrigin()}/${fileName}`);
 }
 
 const getConfigForEnvironment = async (environment) => {
@@ -83,7 +85,7 @@ export const getConfigValue = async (configParam, environment) => {
 /**
  * Retrieves headers from config entries like commerce.headers.pdp.my-header, etc and
  * returns as object of all headers like { my-header: value, ... }
-*/
+ */
 export const getHeaders = async (scope, environment) => {
   const env = environment || calcEnvironment();
   const config = await getConfigForEnvironment(env);
